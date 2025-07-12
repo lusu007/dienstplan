b6ebee2e710c11d01a538556b8eebc21abcf7327
Lukas Jost <lukas.jost@outlook.com>
2025-07-12 18:12:20 +0200

    fix: Add sentry error tracking

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:dienstplan/services/language_service.dart';
 import 'package:dienstplan/utils/logger.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:dienstplan/services/database_service.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,10 +28,25 @@ void main() async {
   await scheduleConfigService.initialize();
   await languageService.initialize();
 
-  runApp(MyApp(
-    scheduleConfigService: scheduleConfigService,
-    languageService: languageService,
-  ));
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://26cd4b7f0d4f1cf36308a96994e7a23a@o4509656380801024.ingest.de.sentry.io/4509656382701648';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+      // The sampling rate for profiling is relative to tracesSampleRate
+      // Setting to 1.0 will profile 100% of sampled transactions:
+      options.profilesSampleRate = 1.0;
+      options.replay.sessionSampleRate = 1.0;
+      options.replay.onErrorSampleRate = 1.0;
+    },
+    appRunner: () => runApp(SentryWidget(
+        child: MyApp(
+      scheduleConfigService: scheduleConfigService,
+      languageService: languageService,
+    ))),
+  );
 }
 
 class AppInitializer extends StatefulWidget {
