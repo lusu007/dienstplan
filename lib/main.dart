06bd8f2c4099117279fa1124651b1e323c9eab8f
Lukas Jost <lukas.jost@outlook.com>
2025-07-21 17:07:38 +0200

    fix: Optimize Sentry config for production (#166)

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,39 @@ import 'package:dienstplan/data/services/language_service.dart';
 import 'package:dienstplan/data/services/sentry_service.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:get_it/get_it.dart';
+import 'package:flutter/foundation.dart';
+
+void configureSentryOptions(
+    SentryFlutterOptions options, SentryService sentryService) {
+  // Default: everything off
+  double tracesSampleRate = 0.0;
+  double profilesSampleRate = 0.0;
+  double replaySessionSampleRate = 0.0;
+  double replayOnErrorSampleRate = 0.0;
+
+  if (sentryService.isEnabled) {
+    if (kReleaseMode) {
+      tracesSampleRate = 0.1;
+      profilesSampleRate = 0.0;
+      if (sentryService.isReplayEnabled) {
+        replaySessionSampleRate = 0.1;
+        replayOnErrorSampleRate = 1.0;
+      }
+    } else {
+      tracesSampleRate = 1.0;
+      profilesSampleRate = 1.0;
+      if (sentryService.isReplayEnabled) {
+        replaySessionSampleRate = 1.0;
+        replayOnErrorSampleRate = 1.0;
+      }
+    }
+  }
+
+  options.tracesSampleRate = tracesSampleRate;
+  options.profilesSampleRate = profilesSampleRate;
+  options.replay.sessionSampleRate = replaySessionSampleRate;
+  options.replay.onErrorSampleRate = replayOnErrorSampleRate;
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -31,27 +64,7 @@ void main() async {
     (options) {
       options.dsn =
           'https://26cd4b7f0d4f1cf36308a96994e7a23a@o4509656380801024.ingest.de.sentry.io/4509656382701648';
-
-      // Apply configuration based on service settings
-      if (sentryService.isEnabled) {
-        options.tracesSampleRate = 1.0;
-        options.profilesSampleRate = 1.0;
-
-        // Only enable replay if both Sentry and replay are enabled
-        if (sentryService.isReplayEnabled) {
-          options.replay.sessionSampleRate = 1.0;
-          options.replay.onErrorSampleRate = 1.0;
-        } else {
-          options.replay.sessionSampleRate = 0.0;
-          options.replay.onErrorSampleRate = 0.0;
-        }
-      } else {
-        // Disable Sentry by setting sample rates to 0
-        options.tracesSampleRate = 0.0;
-        options.profilesSampleRate = 0.0;
-        options.replay.sessionSampleRate = 0.0;
-        options.replay.onErrorSampleRate = 0.0;
-      }
+      configureSentryOptions(options, sentryService);
     },
     appRunner: () => runApp(SentryWidget(
       child: const MyApp(),
