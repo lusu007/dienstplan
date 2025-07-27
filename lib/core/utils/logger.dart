2b95dbd3fb8f4e8949f7a2b8c8ae21dbd6904980
Lukas Jost <lukas.jost@outlook.com>
2025-07-27 12:33:27 +0200

    feat: Send logs to sentry if enabled (#182)

--- lib/core/utils/logger.dart
+++ lib/core/utils/logger.dart
@@ -3,6 +3,9 @@
 import 'dart:io';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:get_it/get_it.dart';
+import 'package:dienstplan/data/services/sentry_service.dart';
 
 class AppLogger {
   static const String _logDirName = 'logs';
@@ -61,6 +64,7 @@ class AppLogger {
       final timestamp = DateTime.now().toIso8601String();
       final logMessage = '[$level] TIME: $timestamp $message';
 
+      // Always print to console for debugging
       if (error != null) {
         print('$logMessage ERROR: $error');
         if (stackTrace != null) {
@@ -70,10 +74,106 @@ class AppLogger {
         print(logMessage);
       }
 
+      // Write to local log file
       await _currentLogFile!
           .writeAsString('$logMessage\n', mode: FileMode.append);
+
+      // Send to Sentry if available and enabled
+      try {
+        // Check if Sentry service is available and enabled
+        final sentryService = GetIt.instance.isRegistered<SentryService>()
+            ? GetIt.instance<SentryService>()
+            : null;
+
+        if (sentryService != null && sentryService.isEnabled) {
+          final sentryLevel = _mapToSentryLevel(level);
+          if (error != null) {
+            Sentry.logger.error(
+              message,
+              attributes: {
+                'local_level': SentryLogAttribute.string(level),
+                'timestamp': SentryLogAttribute.string(timestamp),
+              },
+            );
+          } else {
+            switch (sentryLevel) {
+              case SentryLogLevel.debug:
+                Sentry.logger.debug(
+                  message,
+                  attributes: {
+                    'local_level': SentryLogAttribute.string(level),
+                    'timestamp': SentryLogAttribute.string(timestamp),
+                  },
+                );
+                break;
+              case SentryLogLevel.info:
+                Sentry.logger.info(
+                  message,
+                  attributes: {
+                    'local_level': SentryLogAttribute.string(level),
+                    'timestamp': SentryLogAttribute.string(timestamp),
+                  },
+                );
+                break;
+              case SentryLogLevel.warn:
+                Sentry.logger.warn(
+                  message,
+                  attributes: {
+                    'local_level': SentryLogAttribute.string(level),
+                    'timestamp': SentryLogAttribute.string(timestamp),
+                  },
+                );
+                break;
+              case SentryLogLevel.error:
+                Sentry.logger.error(
+                  message,
+                  attributes: {
+                    'local_level': SentryLogAttribute.string(level),
+                    'timestamp': SentryLogAttribute.string(timestamp),
+                  },
+                );
+                break;
+              case SentryLogLevel.fatal:
+                Sentry.logger.fatal(
+                  message,
+                  attributes: {
+                    'local_level': SentryLogAttribute.string(level),
+                    'timestamp': SentryLogAttribute.string(timestamp),
+                  },
+                );
+                break;
+              default:
+                Sentry.logger.info(
+                  message,
+                  attributes: {
+                    'local_level': SentryLogAttribute.string(level),
+                    'timestamp': SentryLogAttribute.string(timestamp),
+                  },
+                );
+            }
+          }
+        }
+      } catch (sentryError) {
+        // Don't let Sentry errors break local logging
+        print('Failed to send log to Sentry: $sentryError');
+      }
     } catch (e) {
       print('Failed to write log: $e');
+    }
+  }
+
+  static SentryLogLevel _mapToSentryLevel(String level) {
+    switch (level) {
+      case 'D':
+        return SentryLogLevel.debug;
+      case 'I':
+        return SentryLogLevel.info;
+      case 'W':
+        return SentryLogLevel.warn;
+      case 'E':
+        return SentryLogLevel.error;
+      default:
+        return SentryLogLevel.info;
     }
   }
 
